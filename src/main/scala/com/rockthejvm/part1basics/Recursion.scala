db1c89953cfb7566902be4c9474249216898ee3f
Maciej Olszański <olszanskimaciej@wp.pl>
2025-04-15 17:36:58 +0200

    learn recursion - lesson

---
+++ src/main/scala/com/rockthejvm/part1basics/Recursion.scala
@@ -0,0 +1,42 @@
+package com.rockthejvm.part1basics
+
+object Recursion {
+
+  // in functional programing we try to avoid variables and loops, so recursion replaces them
+  def sum(n: Int): Int = {
+    if (n <= 0) then 0
+    else n + sum(n-1)
+  }
+
+
+  //print string exactly N times
+  def printN(string: String, n:Int): Unit = {
+    if (n <= 0) () // () - is empty Unit
+    else {
+      println(string)
+      printN(string, n-1)
+    }
+  }
+
+  //more complex example
+  def isPrime(n: Int): Boolean = {
+    // we need to test all numer from 2 to n/2 whether n % d == 0
+    def testDivisors(d: Int): Boolean = {
+      if (d > n/2) true
+      else if (n % d == 0) false
+      else testDivisors(d + 1)
+    }
+
+    // last expression is the final thing
+    if (n <= 0) false
+    else if (n == 1) false // 1 is not a prime
+    else testDivisors(2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(sum(10))
+    printN("Scala is OK", 3)
+    println(isPrime(7))
+    println(isPrime(8))
+  }
+}
