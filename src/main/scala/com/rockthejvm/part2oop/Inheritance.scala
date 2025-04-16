167826aae99c67f58e91d8a22f336d2fd8fe8a3a
Maciej Olszański <olszanskimaciej@wp.pl>
2025-04-16 16:54:52 +0200

    learn Overriding, Polymorphism and Anonymous classes

--- src/main/scala/com/rockthejvm/part2oop/Inheritance.scala
+++ src/main/scala/com/rockthejvm/part2oop/Inheritance.scala
@@ -26,11 +26,42 @@ object Inheritance {
   val daniel = new Adult()
   val maciek = new Adult_v2("Maciek", 99)
 
+  // overriding = provide a new impl for fields or methods in derived classes
+  class RPG extends VideoGame {
+    override val gameType = "role-playing"
+
+    override def play(): Unit = { // super relates to the parent class
+      println("level up!")
+    }
+  }
+  val wow = new RPG()
+
+  // subtype polymorphism
+  val wow_2: VideoGame = new RPG // declare a value of a parent type, provide and instance of a derived type
+
+  // anonymous classes
+  val psychonauts = new VideoGame {
+    override val gameType = "platformer"
+  }
+  // that's something like this:
+  /*
+    class AnonClass$3274 extends VideoGame {
+      override val gameType = "platformer"
+    }
+
+    val psychonauts: VideoGame = new AnonClass$3274
+  */
+
   def main(args: Array[String]): Unit = {
     println(crysis.gameType)
     crysis.multiplayer
 
     println(daniel.name)
     println(maciek.name)
+
+    println(wow.gameType)
+    wow.play()
+    println(wow_2.gameType)
+    wow_2.play()
   }
 }
