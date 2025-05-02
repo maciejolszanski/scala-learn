81e86bf326b9687bb0a5cae3db7970cc186d89b2
Maciej Olszański <olszanskimaciej@wp.pl>
2025-05-02 12:47:09 +0200

    learn For comprehensions

---
+++ src/main/scala/com/rockthejvm/part3fp/ForComprehensions.scala
@@ -0,0 +1,33 @@
+package com.rockthejvm.part3fp
+
+object ForComprehensions {
+
+  //example
+  val numbers = List(1,2,3,4)
+  val colors = List("black", "white", "blue")
+
+  // ["1-black","1-white","1-blue","2-black", ...]
+  val combinations = numbers.flatMap(n => colors.map(c => s"$n-$c"))
+
+  // it can be also done using for comprehensions
+  val combinations_v2 = for {
+    n <- numbers // generator
+    c <- colors // generator
+  } yield s"$n-$c" // exactly the same as the above
+  // it's not a loop, it's an expression exactly identical with flatMaps and maps!!!
+  // for expression with a bunch of generators mean that those collections (generators will be chained with flatMaps, and the yielded thing will be a map
+
+  /*
+  Exercises - generate a checkerboard from numbers 1-8 and letters A-H
+  */
+  val chessboard = for {
+    letter <- List("A", "B", "C", "D", "E", "F", "G", "H")
+    number <- List(1, 2, 3, 4, 5, 6, 7, 8)
+  } yield s"$letter$number"
+
+  def main(args: Array[String]): Unit = {
+    println(combinations)
+    println(combinations_v2)
+    println(chessboard)
+  }
+}
