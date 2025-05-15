7f5f0715b81f05edab003bd427c6f6d0ce50a628
Maciej Olszański <olszanskimaciej@wp.pl>
2025-05-15 20:06:21 +0200

    learn foldLeft

--- src/main/scala/exercises/sensor/SensorProcessor.scala
+++ src/main/scala/exercises/sensor/SensorProcessor.scala
@@ -5,14 +5,41 @@ object SensorProcessor {
     readings.filter(x => x.isHighTemp(minTemperature))
   }
 
+//  def foldLeft[B](z: B)(op: (B, A) => B): B
+  def avgReadings(readings: List[SensorReading]): Double = {
+    val (sum, count) = readings.foldLeft(0.0, 0){ (accumulator, reading) =>
+      val accSum = accumulator._1
+      val accCount = accumulator._2
+      (accSum + reading.temperature, accCount + 1)
+    }
+
+    if count > 0 then sum / count
+    else 0.0
+  }
+
+  def avgReadings_v2(readings: List[SensorReading]): Double = {
+    val (sum, count) = readings.foldLeft(0.0, 0) {
+      case ((accSum, accCount), reading) => (accSum + reading.temperature, accCount + 1)
+    }
+
+    if count > 0 then sum / count
+    else 0.0
+  }
+
+
   def main(args: Array[String]): Unit = {
     val inputReadings: List[SensorReading] = List(
       SensorReading(1, -1.0),
-      SensorReading(2, 23.0),
-      SensorReading(3, 26.5),
+      SensorReading(2, 23.1),
+      SensorReading(3, 26.0),
       SensorReading(4, 45.4),
       SensorReading(5, 0.0)
     )
-    println(filterReadings(inputReadings, 24.0))
+
+    println(avgReadings_v2(inputReadings))
   }
 }
+
+//Zadanie: Oblicz średnią temperatur z listy SensorReading. Nie używaj wbudowanej funkcji sum.
+//
+//  Cel: foldLeft.
--- src/test/scala/SensorSpec.scala
+++ src/test/scala/SensorSpec.scala
@@ -3,7 +3,7 @@ import exercises.sensor.SensorProcessor
 import org.scalatest.funsuite.AnyFunSuite
 
 class SensorSpec extends AnyFunSuite {
-  val inputReadings: List[SensorReading] = List(
+  val inputReadings1: List[SensorReading] = List(
     SensorReading(1, -1.0),
     SensorReading(2, 23.1),
     SensorReading(3, 26.0),
@@ -12,11 +12,41 @@ class SensorSpec extends AnyFunSuite {
   )
 
   test("filtering") {
-    val actualResult = SensorProcessor.filterReadings(inputReadings, 24.0)
+    val actualResult = SensorProcessor.filterReadings(inputReadings1, 24.0)
     val expectedResult = List(
       SensorReading(3, 26.0),
       SensorReading(4, 45.4)
     )
     assert(actualResult.toSet == expectedResult.toSet)
+  }
+
+  val inputReadings2: List[SensorReading] = List(
+    SensorReading(1, -1.0),
+    SensorReading(2, 0.0),
+    SensorReading(3, 4.0),
+  )
+
+  test("average") {
+    val actualResult = SensorProcessor.avgReadings(inputReadings2)
+    val expectedResult = 1.0
+    assert(actualResult == expectedResult)
+  }
+
+  test("average v2") {
+    val actualResult = SensorProcessor.avgReadings_v2(inputReadings2)
+    val expectedResult = 1.0
+    assert(actualResult == expectedResult)
+  }
+
+  test("average empty list") {
+    val actualResult = SensorProcessor.avgReadings(List())
+    val expectedResult = 0.0
+    assert(actualResult == expectedResult)
+  }
+
+  test("average empty list v2") {
+    val actualResult = SensorProcessor.avgReadings_v2(List())
+    val expectedResult = 0.0
+    assert(actualResult == expectedResult)
   }
 }
